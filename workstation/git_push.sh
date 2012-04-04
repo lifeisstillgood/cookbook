2f49fe790027de22fde8c220ffbd3fec2de26548
Paul Brian <paul@mikadosoftware.com>
2012-04-04 12:11:58 +0100

    some notes on git

--- workstation/git_push.sh
+++ workstation/git_push.sh
@@ -3,13 +3,27 @@
 Set up a remote private git repo...
 ::
 
-  ssh onto yur remote server
+  ssh onto yur remote server paul-brian.com
   $ mkdir private.git
   $ cd private.git
   $ git init --bare
 
   we have now created a bare git repo.  This will by convention be the central
   point for our various different users (basically lots of my machines...)
+
+Now create locally 
+
+  $ mkdir private
+  $ cd private
+  $ echo foo > bar.txt
+  $ git init
+  $ git commit ... etc
+
+  git remote add origin ssh://pbrian@paul-brian.com/home/pbrian/private.git
+
+  git push origin master
+
+  now the remote server paul-brian has a private repo on it, and I can clone and use it to share
 
 
   
